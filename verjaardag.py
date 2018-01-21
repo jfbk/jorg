ff7050244808d2b87c5c256b2d53be5b7685abf9
Fleur Kelpin <f.kelpin@umcg.nl>
2018-01-21 17:32:35 +0100

    Verjaardag met Jorg

---
+++ verjaardag.py
@@ -0,0 +1,8 @@
+import time
+
+naam = input("wat is je naam? ")
+verjaardag = input("en wanneer ben je jarig? ")
+time.sleep(5)
+print("het is", verjaardag)
+time.sleep(5)
+print("Gefeliciteerd", naam)
